da4ef11ca86123ee80187e1ae2f0e0d25ae6786f
manaswinieadara <96780506+manaswinieadara@users.noreply.github.com>
2022-10-06 11:49:12 +0530

    import java.util.Scanner; class divi {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int a,b;         a=sc.nextInt();         b=sc.nextInt();         int div=(a/b);         System.out.println(div);     } }

---
+++ Division.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+class divi
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int a,b;
+        a=sc.nextInt();
+        b=sc.nextInt();
+        int div=(a/b);
+        System.out.println(div);
+    }
+}
