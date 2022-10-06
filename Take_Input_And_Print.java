9eadbbb788b1754eea2efde26059a5a3e406a54b
manaswinieadara <96780506+manaswinieadara@users.noreply.github.com>
2022-10-06 11:55:53 +0530

    import java.util.Scanner; class display {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         String output;         output=sc.nextLine();         System.out.print(output);     } }

---
+++ Take_Input_And_Print.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+class display
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        String output;
+        output=sc.nextLine();
+        System.out.print(output);
+    }
+}
